00de5de9f5206c2e283efa1ebc2ed5eff4f33d58
liortlv <99750449+liortlv@users.noreply.github.com>
2022-05-13 11:37:18 +0300

    Add choosing interface

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const endGameData = [{
 }, {
     name: "power-stone", avengers: ["war-machine", "nebula"]
 }, {
-    name: "time-stone", avengers: [{name: "hulk"}]
+    name: "time-stone", avengers: [{ name: "hulk" }]
 }, {
     name: "soul-stone", avengers: ["black-widow", "hawkeye"]
 }];
@@ -15,3 +15,29 @@ const endGameData = [{
 const stones = document.querySelectorAll('.stone');
 const avengers = document.querySelectorAll('.avenger');
 const glove = document.querySelector('.infinity_glove');
+
+stones.forEach(stone => {
+    stone.addEventListener('click', ({ target }) => {
+        userChoose(stone);
+    });
+});
+
+
+avengers.forEach(avenger => {
+    avenger.addEventListener('click', ({ target }) => {
+        userChoose(avenger);
+    });
+});
+
+glove.addEventListener('click', ({ target }) => {
+    userChoose(glove);
+});
+
+function userChoose(userChoice) {
+    if (userChoice.classList.contains('chosen')) {
+        userChoice.classList.remove('chosen');
+    } else {
+        userChoice.classList.add('chosen');
+    }
+}
+
